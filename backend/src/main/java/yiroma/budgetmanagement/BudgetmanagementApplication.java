130e398535a5b76cd9416e517eef22447761e092
Yiroma <yiromaric@gmail.com>
2026-02-06 17:34:45 +0100

    feat(backend): init backend with Spring Initializr

---
+++ backend/src/main/java/yiroma/budgetmanagement/BudgetmanagementApplication.java
@@ -0,0 +1,13 @@
+package yiroma.budgetmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetmanagementApplication.class, args);
+	}
+
+}
