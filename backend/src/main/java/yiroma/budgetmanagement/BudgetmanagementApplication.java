ca860aa273b9ce1213eedad179570f855a99e262
Yiroma <yiromaric@gmail.com>
2026-02-06 17:52:42 +0100

    fix(bdd): exclude bdd connection

--- backend/src/main/java/yiroma/budgetmanagement/BudgetmanagementApplication.java
+++ backend/src/main/java/yiroma/budgetmanagement/BudgetmanagementApplication.java
@@ -2,12 +2,25 @@ package yiroma.budgetmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		DataSourceAutoConfiguration.class,
+		HibernateJpaAutoConfiguration.class
+})
+@RestController
 public class BudgetmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetmanagementApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello World!";
 	}
 
 }
