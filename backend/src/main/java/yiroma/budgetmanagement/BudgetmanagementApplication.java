13710c144f10c927010552c4f21781aad2eff102
Yiroma <yiromaric@gmail.com>
2026-04-14 18:07:03 +0200

    chore(backend): clean up main application class

--- backend/src/main/java/yiroma/budgetmanagement/BudgetmanagementApplication.java
+++ backend/src/main/java/yiroma/budgetmanagement/BudgetmanagementApplication.java
@@ -2,20 +2,12 @@ package yiroma.budgetmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@RestController
 public class BudgetmanagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BudgetmanagementApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String sayHello() {
-		return "Hello World!";
 	}
 
 }
